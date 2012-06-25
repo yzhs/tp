d8b246ead9385110d2654e8956d0164afad33b80
Colin Benner <cbyzhs@gmail.com>
2012-06-25 16:26:26 +0200

    some experiments on representing typing

--- TP.v
+++ TP.v
@@ -43,12 +43,12 @@ Inductive TPExp :=
 .
 
 Fixpoint TPisvalue (exp : TPExp) : bool :=
- match exp with
- | TPconst _ => true
- | TPabst _ _ => true
- | TPapp (TPconst (TPop _)) exp => TPisvalue exp
- | _ => false
-end.
+  match exp with
+    | TPconst _ => true
+    | TPabst _ _ => true
+    | TPapp (TPconst (TPop _)) exp => TPisvalue exp
+    | _ => false
+  end.
 
 (* Small step semantics *)
 (* {{{ *)
@@ -149,7 +149,7 @@ Fixpoint subst e e' id :=
     | TPlet id' e1 e2 => TPlet id' (subst e1 e' id) (if string_eq id id' then e2 else subst e2 e' id)
     | TPrec id' e => if string_eq id' id then e else TPrec id' (subst e e' id)
   end.
-  
+
 Fixpoint small_step exp :=
   match exp with
     | TPapp (TPabst id e1) e2 =>
@@ -162,36 +162,105 @@ Fixpoint small_step exp :=
           if TPisvalue exp1 then
             if TPisvalue exp2 then
               eval op exp1 exp2                    (* OP *)
-            else TPapp e1 (small_step exp2)        (* APP-RIGHT *)
-          else TPapp (small_step e1) exp2          (* APP-LEFT *)
+              else TPapp e1 (small_step exp2)      (* APP-RIGHT *)
+            else TPapp (small_step e1) exp2        (* APP-LEFT *)
         | _ => if TPisvalue exp2
-                 then TPapp (small_step e1) exp2
-                 else TPapp e1 (small_step exp2)
-       end
+          then TPapp (small_step e1) exp2
+          else TPapp e1 (small_step exp2)
+      end
     | TPif (TPconst (TPbool true)) e2 e3 => e2     (* COND-TRUE *)
     | TPif (TPconst (TPbool false)) e2 e3 => e3    (* COND-FALSE *)
     | TPif e1 e2 e3 => TPif (small_step e1) e2 e3  (* COND-EVAL *)
     | TPlet id e1 e2 =>
       if TPisvalue e1 then
         subst e2 e1 id                             (* LET-EXEC *)
-      else TPlet id (small_step e1) e2             (* LET-EVAL *)
+        else TPlet id (small_step e1) e2           (* LET-EVAL *)
     | TPrec id e => subst e (TPrec id e) id        (* UNFOLD *)
-    | _ => TPconst TPhang
+    | _ => if TPisvalue exp then exp else TPconst TPhang
   end.
 
+Compute small_step (small_step (TPapp (TPabst "x" (TPapp (TPapp (TPconst( TPop TPplus)) (TPid "x")) (TPconst (TPint 1)))) (TPconst (TPint 2)))).
 
 Inductive TPtype :=
-  | TP_unit
-  | TP_bool
-  | TP_int
-  | TP_fun (t1 t2 : TPtype).
+| TP_unit
+| TP_bool
+| TP_int
+| TP_fun (t1 t2 : TPtype)
+| TP_var (id : string)
+| TP_error.
 
 Inductive TPtype_equality :=
-  | TP_eq (t1 t2 : TPtype).
+| TP_eq (t1 t2 : TPtype).
 
-Inductive unify types :=
-  | Triv : forall t lst, ((TP_eq t t) :: lst) -> unify lst.
-(*
-  | Empty nil
-  | Arrow ((TP_eq (TP_fun t1 t2) (TP_fun t1' t2')) :: lst) => unify ((TP_eq t1 t1') :: (TP_eq t2 t2') :: lst).*)
-    
+Inductive Subst' :=
+| Subst (a b : TPtype)
+| Error.
+
+Fixpoint type_eq t1 t2 :=
+  match t1, t2 with
+    | TP_unit, TP_unit => true
+    | TP_bool, TP_bool => true
+    | TP_int, TP_int => true
+    | TP_fun t1 t2, TP_fun t3 t4 => andb (type_eq t1 t3) (type_eq t2 t4)
+    | _, _ => false
+  end.
+
+Fixpoint complexity_type t :=
+  match t with
+    | TP_fun t1 t2 => plus (plus (complexity_type t1) (complexity_type t2)) (1%nat)
+    | _ => 1%nat
+  end.
+
+Definition complexity_type_eq t :=
+  match t with
+    | TP_eq t1 t2 => plus (complexity_type t1) (complexity_type t2)
+  end.
+
+Definition complexity types := fold_left plus (map complexity_type_eq types) 0%nat.
+
+Require Import Recdef.
+
+Function unify (types : list TPtype_equality) {measure complexity} : list Subst' :=
+  match types with
+    | nil => nil
+    | ((TP_eq (TP_fun t1 t2) (TP_fun t1' t2')) :: lst) => unify ((TP_eq t1 t1') :: (TP_eq t2 t2') :: lst)
+    | (TP_eq (TP_var id) t) :: lst => (Subst t (TP_var id)) :: unify lst
+    | (TP_eq t (TP_var id)) :: lst => (Subst t (TP_var id)) :: unify lst
+    | (TP_eq t1 t2) :: lst => if type_eq t1 t2 then unify lst else Error :: nil
+  end.
+Proof.
+  intros.
+  induction lst.
+  now intuition.
+  unfold complexity.
+Admitted.
+
+
+Inductive Typing_judgement :=
+  | tj (env : list (string * TPtype)) (exp : TPExp) (type : TPtype).
+
+Definition typeof op :=
+  match op with
+    | TPplus | TPminus | TPmult | TPdiv | TPmod => TP_fun TP_int (TP_fun TP_int TP_int)
+    | _ => TP_fun TP_int (TP_fun TP_int TP_bool)
+  end.
+
+Fixpoint typing_rules env exp {struct exp} :=
+  match exp with
+    | TPconst TPunit => TP_unit
+    | TPconst (TPbool _) => TP_bool
+    | TPconst (TPint _) => TP_int
+    | TPconst (TPop op) => typeof op
+    | TPconst TPexn => TP_error
+    | TPconst TPhang => TP_error
+    | TPid id => match find (fun a => string_eq id (fst a)) env with | Some (a,b) => b | None => TP_var "" end
+    | TPapp e1 e2 =>
+      match typing_rules env e1 with
+        | TP_fun t1 t2 => if type_eq (typing_rules env e2) t1 then t2 else TP_error
+        | _ => TP_error
+      end
+    | TPabst id e => typing_rules env e
+    | TPif e1 e2 e3 => if negb (type_eq (typing_rules env e1) TP_bool) then TP_error else if negb (type_eq (typing_rules env e2) (typing_rules env e3)) then TP_error else typing_rules env e2
+    | TPlet id e1 e2 => typing_rules ((id, typing_rules env e1) :: env) e2
+    | TPrec id e => typing_rules env e
+  end.
