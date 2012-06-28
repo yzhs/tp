9ca59dd1b25c7e94dae7513639898166c9e97d25
n-chan <n-chan@elvensteel.net>
2012-06-28 11:02:49 +0200

    Added consistency lemma for TPisvalue.

--- TPSyntax.v
+++ TPSyntax.v
@@ -1,14 +1,11 @@
-(* De TP Gedöns *)
+Module TPSyntax.
 
-Require Import ZArith.
-Require Import Syntax.
-Require Import String.
+Require Export ZArith.
+Require Export List.
+Require Export String.
 Require Import Ascii.
-Require Import List.
 
 Open Scope Z_scope.
-
-(* String definition *)
 
 Inductive TPop' :=
  | TPplus
@@ -21,8 +18,7 @@ Inductive TPop' :=
  | TPgreater
  | TPgreatereq
  | TPeq
- | TPneq
-.
+ | TPneq.
 
 Inductive TPconst' :=
  | TPunit
@@ -30,8 +26,7 @@ Inductive TPconst' :=
  | TPint (n : Z)
  | TPop (op : TPop')
  | TPexn
- | TPhang
-.
+ | TPhang.
 
 Inductive TPExp :=
  | TPconst (c : TPconst')
@@ -40,8 +35,7 @@ Inductive TPExp :=
  | TPabst (id : string) (exp : TPExp)
  | TPif (exp1 exp2 exp3 : TPExp)
  | TPlet (id : string) (exp1 exp2 : TPExp)
- | TPrec (id : string) (exp : TPExp)
-.
+ | TPrec (id : string) (exp : TPExp).
 
 Fixpoint TPisvalue (exp : TPExp) : bool :=
   match exp with
@@ -51,25 +45,41 @@ Fixpoint TPisvalue (exp : TPExp) : bool :=
     | _ => false
   end.
 
-(* Small step semantics *)
-(* {{{ *)
-Definition eval op n1 n2 :=
-  match op,  n1, n2 with
-    | TPplus,     TPconst (TPint n), TPconst (TPint n') => TPconst (TPint (n + n'))
-    | TPminus,    TPconst (TPint n), TPconst (TPint n') => TPconst (TPint (n - n'))
-    | TPmult,     TPconst (TPint n), TPconst (TPint n') => TPconst (TPint (n * n'))
-    | TPdiv,      TPconst (TPint n), TPconst (TPint n') => TPconst (TPint (n / n'))
-    | TPmod,      TPconst (TPint n), TPconst (TPint n') => TPconst (TPint (n mod n'))
-    | TPless,     TPconst (TPint n), TPconst (TPint n') => TPconst (TPbool (Zlt_bool n n'))
-    | TPlesseq,   TPconst (TPint n), TPconst (TPint n') => TPconst (TPbool (Zle_bool n n'))
-    | TPgreater,  TPconst (TPint n), TPconst (TPint n') => TPconst (TPbool (Zgt_bool n n'))
-    | TPgreatereq,TPconst (TPint n), TPconst (TPint n') => TPconst (TPbool (Zge_bool n n'))
-    | TPeq,       TPconst (TPint n), TPconst (TPint n') => TPconst (TPbool (Zeq_bool n n'))
-    | TPneq,      TPconst (TPint n), TPconst (TPint n') => TPconst (TPbool (Zneq_bool n n'))
-    | _, _, _ => TPconst TPhang
-  end.
-
-SearchPattern (list _ -> bool).
+Lemma TPisvalue_consist: forall exp, TPisvalue exp = true <-> (exists c, exp = TPconst c) \/ (exists id, exists exp', exp = TPabst id exp') \/ (exists op, exists exp', (exp = TPapp (TPconst (TPop op)) exp' /\ TPisvalue exp' = true)).
+Proof.
+  intros exp. split; intros H.
+  (* => *)
+  induction exp; try (contradict H; discriminate).
+  (* Case exp: TPconst *)
+  left. exists c. now reflexivity.
+  (* Case exp: TPApp *)
+  destruct exp1; simpl in H; try (contradict H; discriminate).
+    (* Case exp1: TPconst *)
+    destruct c; try (contradict H; discriminate).
+      destruct IHexp2.
+        (* Condition: TPisvalue exp2 = true *)
+        exact H.
+        (* Case: First exist is true *)
+        destruct H0. right. right. exists op. exists exp2. split. reflexivity. rewrite H0.  simpl. reflexivity.
+        (* Case: Second or third exist is true *)
+        destruct H0. 
+        (* Case: Second exist is true *)
+        destruct H0. destruct H0. right. right. exists op. exists exp2. split. reflexivity. rewrite H0.  simpl. reflexivity.
+        (* Case: Third exist is true *)
+        destruct H0. destruct H0. destruct H0. right. right. exists op. exists exp2. split. reflexivity. rewrite H0.  simpl. exact H1.
+  (* Case exp: TPabst *)
+  right. left. exists id. exists exp. reflexivity.
+  (* <= *)
+  destruct H.
+    (* Case: First exist is true *)
+    destruct H. rewrite H. compute. now reflexivity.
+    (* Case: Second or third exist is true *)
+    destruct H.
+    (* Case: Second exist is true *)
+    destruct H. destruct H. rewrite H. compute. now reflexivity.
+    (* Case: Third exist is true *)
+    destruct H. destruct H. destruct H. rewrite H. simpl. exact H0.
+Qed.
 
 (* Boolean version of operator equality *)
 Definition TPop_eq op1 op2 :=
@@ -153,10 +163,6 @@ Proof.
     (* Case TPhang*)
     now reflexivity.
 Qed.
-
-(* }}} *)
-
-(*Definition string_eq s1 s2 := andb (prefix s1 s2) (prefix s2 s1).*)
 
 (* Very ugly boolean version of ascii equality *)
 Definition ascii_eq a1 a2 := match a1, a2 with
@@ -339,179 +345,4 @@ Proof.
     simpl. now reflexivity.
 Qed.
 
-(* TODO: rewrite using sets (ListSet?) *)
-Fixpoint free exp :=
-  match exp with
-    | TPconst _ => nil
-    | TPid id => id :: nil
-    | TPapp exp1 exp2 => app (free exp1) (free exp2)
-    | TPif exp1 exp2 exp3 => app (free exp1) (app (free exp2) (free exp3))
-    | TPabst id exp => filter (string_neq id) (free exp)
-    | TPlet id e1 e2 => app (filter (string_neq id) (free e2)) (free e1)
-    | TPrec id exp => filter (string_neq id) (free exp)
-  end.
-
-(*
-Fixpoint new_id free_ids id :=
-  
-
-Fixpoint subst free_ids e e' id :=
-  match e with
-    | TPconst c => c
-    | TPid id' => if string_eq id' id then e' else id'
-    | TPabst id' e => if string_eq id' id then e else
-      let id'' := new_id free_ids id' in TPabst id'' (subst (id'' :: free_ids) (subst free_ids e' (TPid id'') id) e' id)
-    | _ => TPconst TPunit
-  end.
-*)
-
-Fixpoint subst e e' id :=
-  match e with
-    | TPconst c => e
-    | TPid id' => if string_eq id' id then e' else TPid id'
-    | TPif e1 e2 e3 => TPif (subst e1 e' id) (subst e2 e' id) (subst e3 e' id)
-    | TPapp e1 e2 => TPapp (subst e1 e' id) (subst e2 e' id)
-    | TPabst id' e => if string_eq id' id then e else TPabst id' (subst e e' id)
-    | TPlet id' e1 e2 => TPlet id' (subst e1 e' id) (if string_eq id id' then e2 else subst e2 e' id)
-    | TPrec id' e => if string_eq id' id then e else TPrec id' (subst e e' id)
-  end.
-
-Fixpoint small_step exp :=
-  match exp with
-    | TPapp (TPabst id e1) e2 =>
-      if TPisvalue e2
-        then subst e1 e2 id                        (* BETA-V *)
-        else TPapp (small_step e1) e2              (* APP-LEFT *)
-    | TPapp e1 exp2 =>
-      match e1 with
-        | TPapp (TPconst (TPop op)) exp1 =>
-          if TPisvalue exp1 then
-            if TPisvalue exp2 then
-              eval op exp1 exp2                    (* OP *)
-              else TPapp e1 (small_step exp2)      (* APP-RIGHT *)
-            else TPapp (small_step e1) exp2        (* APP-LEFT *)
-        | _ => if TPisvalue exp2
-          then TPapp (small_step e1) exp2
-          else TPapp e1 (small_step exp2)
-      end
-    | TPif (TPconst (TPbool true)) e2 e3 => e2     (* COND-TRUE *)
-    | TPif (TPconst (TPbool false)) e2 e3 => e3    (* COND-FALSE *)
-    | TPif e1 e2 e3 => TPif (small_step e1) e2 e3  (* COND-EVAL *)
-    | TPlet id e1 e2 =>
-      if TPisvalue e1 then
-        subst e2 e1 id                             (* LET-EXEC *)
-        else TPlet id (small_step e1) e2           (* LET-EVAL *)
-    | TPrec id e => subst e (TPrec id e) id        (* UNFOLD *)
-    | _ => if TPisvalue exp then exp else TPconst TPhang
-  end.
-
-Compute small_step (small_step (TPapp (TPabst "x" (TPapp (TPapp (TPconst( TPop TPplus)) (TPid "x")) (TPconst (TPint 1)))) (TPconst (TPint 2)))).
-
-Inductive TPtype :=
-| TP_unit
-| TP_bool
-| TP_int
-| TP_fun (t1 t2 : TPtype)
-| TP_var (id : string)
-| TP_error.
-
-Inductive TPtype_equality :=
-| TP_eq (t1 t2 : TPtype).
-
-Inductive Subst' :=
-| Subst (a b : TPtype)
-| Error.
-
-Fixpoint type_eq t1 t2 :=
-  match t1, t2 with
-    | TP_unit, TP_unit => true
-    | TP_bool, TP_bool => true
-    | TP_int, TP_int => true
-    | TP_fun t1 t2, TP_fun t3 t4 => andb (type_eq t1 t3) (type_eq t2 t4)
-    | TP_var id1, TP_var id2 => string_eq id1 id2
-    | TP_error, TP_error => true
-    | _, _ => false
-  end.
-
-Lemma type_eq_consist: forall t1 t2: TPtype, type_eq t1 t2 = true <-> t1 = t2.
-Proof.
-  intros t1 t2. split; intros H.
-  (* => *)
-    generalize dependent t2.
-    induction t1; destruct t2; intros H; try (discriminate || reflexivity).
-    (* Case: TP_fun *)
-    simpl in H. apply andb_prop in H. destruct H as [H H'].
-    rewrite IHt1_1 with (t2:=t2_1).
-    rewrite IHt1_2 with (t2:=t2_2).
-    now reflexivity. now exact H'. now exact H.
-    (* Case: TP_var *)
-    simpl in H. apply string_eq_consist in H. rewrite H.
-    now reflexivity.
-  (* <= *)
-    rewrite H. clear H. clear t1.
-    induction t2; simpl; try reflexivity.
-    (* Case: TP_fun *)
-    rewrite IHt2_1. rewrite IHt2_2. simpl. now reflexivity.
-    (* Case: TP_var *)
-    apply string_eq_reflex.
-Qed.  
-
-Fixpoint complexity_type t :=
-  match t with
-    | TP_fun t1 t2 => plus (plus (complexity_type t1) (complexity_type t2)) (1%nat)
-    | _ => 1%nat
-  end.
-
-Definition complexity_type_eq t :=
-  match t with
-    | TP_eq t1 t2 => plus (complexity_type t1) (complexity_type t2)
-  end.
-
-Definition complexity types := fold_left plus (map complexity_type_eq types) 0%nat.
-
-Require Import Recdef.
-
-Function unify (types : list TPtype_equality) {measure complexity} : list Subst' :=
-  match types with
-    | nil => nil
-    | ((TP_eq (TP_fun t1 t2) (TP_fun t1' t2')) :: lst) => unify ((TP_eq t1 t1') :: (TP_eq t2 t2') :: lst)
-    | (TP_eq (TP_var id) t) :: lst => (Subst t (TP_var id)) :: unify lst
-    | (TP_eq t (TP_var id)) :: lst => (Subst t (TP_var id)) :: unify lst
-    | (TP_eq t1 t2) :: lst => if type_eq t1 t2 then unify lst else Error :: nil
-  end.
-Proof.
-  intros.
-  induction lst.
-  now intuition.
-  unfold complexity.
-Admitted.
-
-
-Inductive Typing_judgement :=
-  | tj (env : list (string * TPtype)) (exp : TPExp) (type : TPtype).
-
-Definition typeof op :=
-  match op with
-    | TPplus | TPminus | TPmult | TPdiv | TPmod => TP_fun TP_int (TP_fun TP_int TP_int)
-    | _ => TP_fun TP_int (TP_fun TP_int TP_bool)
-  end.
-
-Fixpoint typing_rules env exp {struct exp} :=
-  match exp with
-    | TPconst TPunit => TP_unit
-    | TPconst (TPbool _) => TP_bool
-    | TPconst (TPint _) => TP_int
-    | TPconst (TPop op) => typeof op
-    | TPconst TPexn => TP_error
-    | TPconst TPhang => TP_error
-    | TPid id => match find (fun a => string_eq id (fst a)) env with | Some (a,b) => b | None => TP_var "" end
-    | TPapp e1 e2 =>
-      match typing_rules env e1 with
-        | TP_fun t1 t2 => if type_eq (typing_rules env e2) t1 then t2 else TP_error
-        | _ => TP_error
-      end
-    | TPabst id e => typing_rules env e
-    | TPif e1 e2 e3 => if negb (type_eq (typing_rules env e1) TP_bool) then TP_error else if negb (type_eq (typing_rules env e2) (typing_rules env e3)) then TP_error else typing_rules env e2
-    | TPlet id e1 e2 => typing_rules ((id, typing_rules env e1) :: env) e2
-    | TPrec id e => typing_rules env e
-  end.
+End TPSyntax.
